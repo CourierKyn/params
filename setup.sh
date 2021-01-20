303e57cf598e3164f5310258c72d33750829798c
Courier Kyn <franklin_a_h_p@qq.com>
2021-01-20 15:06:46 +0800

    A little fix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 python3 -m pip install --upgrade setuptools wheel
-rm -rf dist/* && python3 setup.py sdist bdist_wheel
+rm -rf "dist/*" && python3 setup.py sdist bdist_wheel
 python3 -m pip install --upgrade twine
 python3 -m twine upload dist/*
